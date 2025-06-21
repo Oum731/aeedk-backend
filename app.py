873af6a7c6f1684afc29a7d5ee36d925d94c5fde
MEURSAULT A <Traore Souroge Oumar>
2025-06-21 13:46:49 +0100

    Force redeploy on Render

--- app.py
+++ app.py
@@ -46,7 +46,12 @@ def create_app():
         MAIL_PASSWORD=os.getenv('MAIL_PASSWORD'),
         MAIL_DEFAULT_SENDER=os.getenv('MAIL_DEFAULT_SENDER'),
         MAIL_DEBUG=True
+
     )
+    app.config["SQLALCHEMY_ENGINE_OPTIONS"] = {
+        "pool_recycle": 280,
+        "pool_pre_ping": True
+    }
 
     db.init_app(app)
     bcrypt.init_app(app)
