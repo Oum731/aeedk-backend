459daf3a083170844e00605595b4992431fdb4c0
MEURSAULT A <Traore Souroge Oumar>
2025-06-21 01:47:18 +0100

    Force redeploy on Render

--- app.py
+++ app.py
@@ -21,19 +21,20 @@ def create_app():
     )
 
     app.config.update(
-        SECRET_KEY=os.getenv('SECRET_KEY'),
-        JWT_SECRET_KEY=os.getenv('JWT_SECRET_KEY'),
-        JWT_ACCESS_TOKEN_EXPIRES=timedelta(hours=12),
-        SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI'),
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        MAIL_SERVER=os.getenv('MAIL_SERVER', 'smtp.gmail.com'),
-        MAIL_PORT=int(os.getenv('MAIL_PORT', 587)),
-        MAIL_USE_TLS=os.getenv('MAIL_USE_TLS', 'True') == 'True',
-        MAIL_USERNAME=os.getenv('MAIL_USERNAME'),
-        MAIL_PASSWORD=os.getenv('MAIL_PASSWORD'),
-        MAIL_DEFAULT_SENDER=os.getenv('MAIL_DEFAULT_SENDER'),
-        MAIL_DEBUG=True
-    )
+    SECRET_KEY=os.getenv('SECRET_KEY'),
+    JWT_SECRET_KEY=os.getenv('JWT_SECRET_KEY'),
+    JWT_ACCESS_TOKEN_EXPIRES=timedelta(hours=12),
+    SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI', 'mysql+pymysql://root:@localhost/pfe_db'),
+    SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    MAIL_SERVER=os.getenv('MAIL_SERVER', 'smtp.gmail.com'),
+    MAIL_PORT=int(os.getenv('MAIL_PORT', 587)),
+    MAIL_USE_TLS=os.getenv('MAIL_USE_TLS', 'True') == 'True',
+    MAIL_USERNAME=os.getenv('MAIL_USERNAME'),
+    MAIL_PASSWORD=os.getenv('MAIL_PASSWORD'),
+    MAIL_DEFAULT_SENDER=os.getenv('MAIL_DEFAULT_SENDER'),
+    MAIL_DEBUG=True
+)
+
 
     db.init_app(app)
     bcrypt.init_app(app)
